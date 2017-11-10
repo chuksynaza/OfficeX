b1373b46a82c124f84050bbc4069061058224527
chuksynaza <nazachuksy@gmail.com>
2017-11-10 13:38:22 +0100

    Added Demo Chat and file transfer pages

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
 var express = require('express');
 var app = express();
 var ExpressPeerServer = require('peer').ExpressPeerServer;
+var fs = require('fs');
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res, next) { res.send('Hello world!'); });
+
+app.get('/send', function(req, res, next) { 
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('playb.html', function (err, data){
+		if(err) {
+			throw error;
+		}
+		else {
+			res.write(data);
+		}
+
+		res.end();
+
+	});
+
+});
+
+app.get('/recieve', function(req, res, next) { 
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('playc.html', function (err, data){
+		if(err) {
+			throw error;
+		}
+		else {
+			res.write(data);
+		}
+
+		res.end();
+
+	});
+
+});
 
 var server = app.listen(9000);
 
 var options = {
-    debug: true
+	debug: true
 }
 var connServer = ExpressPeerServer(server, options);
 app.use('/officeConn', connServer);
 
 connServer.on('connection', function(id) {
-    console.log('New connection with '+id)
+	console.log('New connection with '+id)
 });
 
 connServer.on('disconnect', function (id) {
-  console.log('disconnect with id ' + id);
+	console.log('disconnect with id ' + id);
 });
